f32a06411fe2db208ed7b0209264225eee097072
Garv Jain <garv.jainn@gmail.com>
2024-09-27 00:01:04 -0700

    added robust features

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,65 +1,16 @@
-'use client';
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import {useState, useEffect, Suspense} from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Form from '@components/Form';
+const EditPrompt = dynamic(() => import("@components/EditPrompt"), {
+  suspense: true,
+});
 
-const EditPrompt = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const promptId = searchParams.get('id');
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: '',
-    });
-    useEffect(() => {
-        const getPromptDetails = async () => {
-            const response = await fetch(`/api/prompt/${promptId}`)
-            const data = await response.json();
-            console.log(data);
-            setPost({
-                prompt: data.prompt,
-                tag:data.tag,
-            })
-
-        }
-        if(promptId) getPromptDetails()
-    }, [promptId])
-    const updatePrompt = async(e) => {
-        e.preventDefault(); //prevent default behavior of the website after submitting (reload)
-        setSubmitting(true);
-        
-        if(!promptId) return alert('Prompt ID not found')
-        try {
-            const response = await fetch(`/api/prompt/${promptId}`, {
-                method: 'PATCH',
-                body: JSON.stringify({
-                    prompt: post.prompt,
-                    tag: post.tag
-                })
-            })
-            if(response.ok){
-                router.push('/');
-            }
-        } catch (error){
-            console.log(error)
-        } finally {
-            setSubmitting(false);
-        }
-    }
-    return (
+const Page = () => {
+  return (
     <Suspense>
-        <Form
-            type = "Edit"
-            post = {post}
-            setPost = {setPost}
-            submitting = {submitting}
-            handleSubmit = {updatePrompt}
-        
-        />
+      <EditPrompt />
     </Suspense>
-  )
-}
+  );
+};
 
-export default EditPrompt
+export default Page;
